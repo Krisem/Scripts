1a3634af008a81c184a3e8eeb9feba12f53d28fc
Krisem <semelenge@gmail.com>
2016-02-29 17:14:02 +0100

    Panda med XLRD

--- Pandas/Py2Pandatest.py
+++ Pandas/Py2Pandatest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # read in data using the many mathods to do so, making the student the unique identifier
-marks = pd.read_csv('Input/Grade.csv', index_col='student')
+marks = pd.read_excel('Input/Grade.xlsx', index_col='Student')
 
 # gets the first question column, if the column didn't have spaces, you
 # could have used dot look up: marks.question1
@@ -15,7 +15,7 @@ marks['question 1'] == 'y'
 # (and: &) and (or: |) I don't know why they didn't implement it as
 # && and ||, but they must have their reasons
 # parentheses are important here
-#(marks['question 1'] == 'y') & (marks['question 2'] == 'n')
+(marks['question 1'] == 'y') & (marks['question 2'] == 'n')
 
 # using a series of booleans as a look-up will create a new
 # dataframe with just the matches, therefore this filters all the
@@ -26,5 +26,5 @@ print marks[(marks['question 1'] == 'y') & (marks['question 2'] == 'n')]
 # you can then lookup what you want from the result in this case,
 # look up the answers students gave for question 4, if they answered
 # 'y' for question 1 and 'n' for questions 2.
-#marks[(marks['question 1'] == 'y') & (marks['question 2'] == 'n')]['question 4']
+marks[(marks['question 1'] == 'y') & (marks['question 2'] == 'n')]['question 4']
 
