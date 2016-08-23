68bc62205a5ba819cca2aca44f8eb32aaed84375
Krisem <semelenge@gmail.com>
2016-08-23 15:43:18 +0200

    rot
    Klarer fortsatt ikke write to file

--- Pandas/PyHashEx2.py
+++ Pandas/PyHashEx2.py
@@ -1,11 +1,17 @@
-import os
-import hashlib
-import unicodedata
-# -*- coding: utf-8 -*-
+import hashlib, csv
+
 
 with open("Output/email.txt","r") as f:
     for line in f.readlines():
         line = line.rstrip("\n")
         line_uni = line.encode("utf-8")
         m = hashlib.sha256(line_uni)
-        print(m.hexdigest())
+        #print(m.hexdigest())
+        #print(m.digest_size)
+    f.close()
+    with open("Result2.csv", "a") as hmails:
+        #for line in m.hexdigest:
+        #hashedmails.write("%s\n" % item)
+            hmails.write(m.hexdigest())
+    hmails.close()
+
